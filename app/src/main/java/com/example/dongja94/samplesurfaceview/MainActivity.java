a15787f1cffb0f497884def12a026c9aa93d3a47
dongja94 <dongja94@gmail.com>
2016-02-03 11:05:58 +0900

    Initial commit

---
+++ app/src/main/java/com/example/dongja94/samplesurfaceview/MainActivity.java
@@ -0,0 +1,163 @@
+package com.example.dongja94.samplesurfaceview;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        SurfaceView surfaceView = (SurfaceView)findViewById(R.id.surfaceView);
+        surfaceView.getHolder().addCallback(this);
+
+        isRunning = true;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                drawCircleAnimation();
+            }
+        }).start();
+    }
+
+    Object drawLock = new Object();
+
+    Paint mPaint = new Paint();
+
+    int radius = 0;
+    boolean isRunning = false;
+    private void drawCircleAnimation() {
+        mPaint.setColor(Color.GREEN);
+        while(isRunning) {
+//            while(mSurface == null && isRunning) {
+//                synchronized (drawLock) {
+//                    try {
+//                        drawLock.wait();
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }
+//
+//            if (!isRunning) {
+//                return;
+//            }
+
+            Canvas canvas = null;
+            if (mSurface == null) {
+                try {
+                    Thread.sleep(20);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                continue;
+            }
+            try {
+                canvas = mSurface.lockCanvas(null);
+                drawCircle(canvas);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (canvas != null) {
+                        mSurface.unlockCanvasAndPost(canvas);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void drawCircle(Canvas canvas) {
+        canvas.drawColor(Color.WHITE);
+
+        canvas.drawCircle(300, 300, radius, mPaint);
+
+        radius += 3;
+        if (radius > 200) {
+            radius = 0;
+        }
+    }
+
+    Surface mSurface;
+
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        mSurface = holder.getSurface();
+        drawNotify();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        mSurface = holder.getSurface();
+        drawNotify();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        mSurface = null;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        isRunning = false;
+        drawNotify();
+    }
+
+    void drawNotify() {
+//        try {
+//            drawLock.notifyAll();
+//        } catch (IllegalMonitorStateException e) {
+//
+//        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
